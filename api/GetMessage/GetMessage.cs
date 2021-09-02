0b65a755192312d57514f8da1af05371f6d9095d
Michail Shaposhnikov <mishapos@microsoft.com>
2021-09-02 14:03:18 -0700

    Added Function to read content and return value

--- api/GetMessage/GetMessage.cs
+++ api/GetMessage/GetMessage.cs
@@ -18,18 +18,8 @@ namespace StaticWebAppsEndToEndTesting.GetMessage
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            string newContent = await File.ReadAllTextAsync("content.txt");
+            return new OkObjectResult(newContent);
         }
     }
 }
