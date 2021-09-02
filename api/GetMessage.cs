a4e030650ecea41dbdb11e26c48393a33946fbe9
static-web-apps-machine-user <84103011+static-web-apps-machine-user@users.noreply.github.com>
2021-09-02 16:28:46 -0700

    Update GetMessage.cs

--- api/GetMessage.cs
+++ api/GetMessage.cs
@@ -17,8 +17,13 @@ namespace StaticWebAppsEndToEndTesting.GetMessage
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-            string newContent = await File.ReadAllTextAsync("content.txt");
+            log.LogInformation("C# HTTP trigger function processed a request! :) ");
+            log.LogInformation($"CWD: {System.IO.Directory.GetCurrentDirectory()}");
+            string[] allfiles = Directory.GetFiles(System.IO.Directory.GetCurrentDirectory(), "*.*", SearchOption.AllDirectories);
+            foreach (var file in allfiles){
+                 log.LogInformation($"File: {file}");
+            }
+            string newContent = await File.ReadAllTextAsync(Path.Combine(System.IO.Directory.GetCurrentDirectory(),"content.txt"));
             return new OkObjectResult(newContent);
         }
     }
