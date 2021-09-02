319624f4b65849be0cc67eed186194069572edff
Michail Shaposhnikov <mishapos@microsoft.com>
2021-09-02 16:40:39 -0700

    Try adding as embedded resource

--- api/GetMessage.cs
+++ api/GetMessage.cs
@@ -17,14 +17,19 @@ namespace StaticWebAppsEndToEndTesting.GetMessage
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
+            string message = "";
             log.LogInformation("C# HTTP trigger function processed a request! :) ");
-            log.LogInformation($"CWD: {System.IO.Directory.GetCurrentDirectory()}");
-            string[] allfiles = Directory.GetFiles(System.IO.Directory.GetCurrentDirectory(), "*.*", SearchOption.AllDirectories);
-            foreach (var file in allfiles){
-                 log.LogInformation($"File: {file}");
+            // Determine path
+            var assembly = Assembly.GetExecutingAssembly();
+            // Format: "{Namespace}.{Folder}.{filename}.{Extension}"
+            string resourcePath = assembly.GetManifestResourceNames().Single(str => str.EndsWith("content.txt"));
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourcePath))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                message = reader.ReadToEnd();
             }
-            string newContent = await File.ReadAllTextAsync(Path.Combine(System.IO.Directory.GetCurrentDirectory(),"content.txt"));
-            return new OkObjectResult(newContent);
+            return new OkObjectResult(message);
         }
     }
 }
